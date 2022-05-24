bc4e0db70730fa54c236979c84f777e48a8ebe1e
sundaravadivelcp <sundaravadivelcp@gmail.com>
2022-05-24 00:41:42 -0700

    Time: 50 ms (72.07%), Space: 17.6 MB (29.10%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,23 +1,44 @@
 class Solution {
-public:
-    static bool sortcol(const vector<int>& v1, const vector<int>& v2)
-    {
-        return v1[1] > v2[1];
+private:
+    static bool cmp(pair<int,int>a, pair<int,int>b){
+        return a.second>b.second;
     }
+public:
+//     static bool sortcol(const vector<int>& v1, const vector<int>& v2)
+//     {
+//         return v1[1] > v2[1];
+//     }
     
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+//         int sum = 0;
+        
+//         sort(boxTypes.begin(), boxTypes.end(), sortcol);
+        
+//         int i=0; 
+//         while(truckSize > 0 && i < boxTypes.size()){
+//             int m = min(truckSize , boxTypes[i][0]);
+//             sum += m * boxTypes[i][1];
+//             truckSize -= m;
+//             i++;
+//         }
+        
+//         return sum;
         int sum = 0;
+        map<int, int, greater<int>> m;
         
-        sort(boxTypes.begin(), boxTypes.end(), sortcol);
-        
-        int i=0; 
-        while(truckSize > 0 && i < boxTypes.size()){
-            int m = min(truckSize , boxTypes[i][0]);
-            sum += m * boxTypes[i][1];
-            truckSize -= m;
-            i++;
+        for(auto& box : boxTypes){
+            m[box[1]] += box[0];
         }
         
+        for(auto it = m.begin(); it!= m.end(); ++it) {
+            if(it->second < truckSize){
+                sum += (it->first * it->second);
+                truckSize -= it->second;    
+            } else {
+                sum += (truckSize * it->first);
+                break;
+            }
+        }
         return sum;
     }
 };
