7348f49fefac0fd9c174ae072bf48c4e12533061
sundaravadivelcp <sundaravadivelcp@gmail.com>
2022-05-24 03:00:19 -0700

    Time: 6 ms (8.68%), Space: 5.8 MB (83.44%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int f[38] = {0};
+        int ans;
+        f[0] = 0;
+        f[1] = 1;
+        f[2] = 1;
+        for(int i=3; i <= n; i++){
+            f[i] = f[i-1] + f[i-2] + f[i-3];
+        }
+        
+        return f[n];
+    }
+};
