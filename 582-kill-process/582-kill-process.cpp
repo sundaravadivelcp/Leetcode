5652f7a6e7680fdece9d2bc716a43ed3f6f42f64
sundaravadivelcp <sundaravadivelcp@gmail.com>
2022-05-25 07:20:16 -0700

    Time: 690 ms (5.13%), Space: 53.8 MB (99.05%) - LeetHub

---
+++ 582-kill-process/582-kill-process.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> killProcess(vector<int>& pid, vector<int>& ppid, int kill) {
+        vector<int> ans;
+        
+        stack<int> s;
+        
+        map<int, int> m;
+        
+        s.push(kill);
+        
+        ans.push_back(kill);
+        
+        for(auto& parent : ppid){
+            m[parent]++;
+        }
+        
+        int n = ppid.size(), j = 0, count = 1;
+        while(!s.empty()){
+            kill = s.top();
+            s.pop();
+            for(int i=0; i<n; i++){
+                if(ppid[i] == kill){
+                    ans.push_back(pid[i]);
+                    if(m.count(pid[i])){
+                        s.push(pid[i]);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
