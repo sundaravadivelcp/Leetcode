590ee8ee0bc0b18960cea69b28d7c6f38c445d26
sundaravadivelcp <sundaravadivelcp@gmail.com>
2022-05-27 04:31:17 -0700

    Time: 95 ms (83.36%), Space: 22.9 MB (79.71%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int connectSticks(vector<int>& sticks) {
-        priority_queue <int, vector<int>, greater<int> > pq;
+        priority_queue <int, vector<int>, greater<int> > pq(sticks.begin(),sticks.end());
         int n = sticks.size();
         if(n==1) return 0;
         if(n==2) return sticks[0] + sticks[1];
 
         long long int sum = 0, cost = 0;
-        for(int i=0;i<sticks.size();i++){
-            pq.push(sticks[i]);
-        }
+        // for(int i=0;i<sticks.size();i++){
+        //     pq.push(sticks[i]);
+        // }
         
         while(pq.size()>1){
             int n1 = pq.top();
