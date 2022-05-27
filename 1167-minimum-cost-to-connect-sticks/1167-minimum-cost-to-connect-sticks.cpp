536168f0d3684583cad6032d03d303c50866fe36
sundaravadivelcp <sundaravadivelcp@gmail.com>
2022-05-27 04:23:27 -0700

    Time: 98 ms (76.23%), Space: 22.8 MB (88.43%) - LeetHub

---
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int connectSticks(vector<int>& sticks) {
+        priority_queue<int, vector<int>, greater<int>> qu(sticks.begin(), sticks.end());
+        int result = 0;
+        while (qu.size() > 1) {
+            int a = qu.top(); qu.pop();
+            int b = qu.top(); qu.pop();
+            result += a + b;
+            qu.push(a + b);
+        }
+        return result;
+            
+    }
+};
