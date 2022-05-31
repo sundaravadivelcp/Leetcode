ad1bc14bcdfa49fe2e08b2fba609678ec9b7a93e
sundaravadivelcp <sundaravadivelcp@gmail.com>
2022-05-31 03:07:57 -0700

    Time: 17 ms (65.35%), Space: 13.9 MB (40.62%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    int hrob(vector<int>& nums, int s, int e){
-        int n = e - s;
-        if(n == 1)
-            return nums[0+s];
-        if(n==2)
-            return max(nums[0+s], nums[1+s]);
-        int dp[n];
-        dp[0] = nums[0+s];
-        dp[1] = max(nums[0+s],nums[1+s]);
-        cout<< dp[0]<<" " <<dp[1]<<" ";
-        for(int i=2; i < n; i++){
-            dp[i] = max(nums[i+s] + dp[i-2],dp[i-1]);
-        }
-        cout<<endl;
-        return dp[n-1];
-    }
+    int dp[10001];
     
-    int deleteAndEarn(vector<int>& nums) {
-        vector<int> a(10001,0);
-        int max = INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            a[nums[i]] += nums[i];
-            if(max < nums[i])
-                max = nums[i];
-        }
+    int deleteAndEarn(vector<int>& nums) 
+    {
+        memset(dp, 0, sizeof dp);
+        vector<int> sum(10001, 0);
+        for(auto num : nums) sum[num] += num;
         
-        if(nums.size()==1){
-            return nums[0];
-        } else {
-            return hrob(a,0,max+1);
-        }
+        dp[1] = sum[1];
+        
+        for(int i=2; i<sum.size(); i++)
+            dp[i] = max(sum[i] + dp[i-2], dp[i-1]);
+
+        return dp[sum.size()-1];
     }
 };
