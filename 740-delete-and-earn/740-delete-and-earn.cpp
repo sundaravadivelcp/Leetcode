540d6800184b4d0bb7e181f5538b61d6cedbca61
sundaravadivelcp <sundaravadivelcp@gmail.com>
2022-05-31 03:02:28 -0700

    Time: 21 ms (50.48%), Space: 16.2 MB (26.00%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int hrob(vector<int>& nums, int s, int e){
+        int n = e - s;
+        if(n == 1)
+            return nums[0+s];
+        if(n==2)
+            return max(nums[0+s], nums[1+s]);
+        int dp[n];
+        dp[0] = nums[0+s];
+        dp[1] = max(nums[0+s],nums[1+s]);
+        cout<< dp[0]<<" " <<dp[1]<<" ";
+        for(int i=2; i < n; i++){
+            dp[i] = max(nums[i+s] + dp[i-2],dp[i-1]);
+        }
+        cout<<endl;
+        return dp[n-1];
+    }
+    
+    int deleteAndEarn(vector<int>& nums) {
+        vector<int> a(20001,0);
+        int max = INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            a[nums[i]] += nums[i];
+            if(max < nums[i])
+                max = nums[i];
+        }
+        
+        if(nums.size()==1){
+            return nums[0];
+        } else {
+            return hrob(a,0,max+1);
+        }
+    }
+};
