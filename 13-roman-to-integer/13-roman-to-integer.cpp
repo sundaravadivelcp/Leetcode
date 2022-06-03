f97d2bc0b745288dbb7bce194c562b898729ac27
sundaravadivelcp <sundaravadivelcp@gmail.com>
2022-06-03 02:45:04 -0700

    Time: 17 ms (46.41%), Space: 7.8 MB (46.53%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -3,7 +3,7 @@ public:
     int romanToInt(string s) {
         int ans = 0, n = s.length();
         
-        map<char,int> m = {{'M',1000},{'D',500},{'C',100},{'L',50},{'X',10},{'V',5},{'I',1}};
+        unordered_map<char,int> m = {{'M',1000},{'D',500},{'C',100},{'L',50},{'X',10},{'V',5},{'I',1}};
         
         for(int i = 0;i<n-1;i++){
             if(m[s[i+1]] > m[s[i]]){
