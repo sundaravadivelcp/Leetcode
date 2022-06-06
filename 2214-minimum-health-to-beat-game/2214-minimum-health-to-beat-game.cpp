65abda1d40b3cc80d7a8c41102352d7f69a2b1bf
sundaravadivelcp <sundaravadivelcp@gmail.com>
2022-06-06 15:17:51 -0700

    Time: 136 ms (42.16%), Space: 59.2 MB (65.33%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.cpp
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     long long minimumHealth(vector<int>& damage, int armor) {
         long long ans=1;
-        int m = 0,mi = -1,f=0;
+        int m = 0;
         if(armor > 0){
             for(int i=0;i<damage.size();i++){
                 if(m<damage[i]){
                     m = max(m,damage[i]);
-                    mi = i;
                 }
                 if(m>=armor)
                     break;
