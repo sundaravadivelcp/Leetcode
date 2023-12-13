99e5b31dae0df46c6a0e36554ef730fba9e87e89
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2023-12-12 19:23:49 -0600

    Time: 8 ms (80.81%), Space: 22.4 MB (44.65%) - LeetHub

---
+++ 1120-maximum-average-subtree/1120-maximum-average-subtree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    double ans = 0;
+
+    pair<int, int> preorder(TreeNode* root){
+        if(!root){
+            return {0,0};
+        }
+        pair<int,int> left = preorder(root->left);
+        pair<int,int> right = preorder(root->right);
+        int sum = left.first + right.first + root->val;
+        int num = left.second + right.second + 1;
+        ans = max((double)sum/(double) num, ans);
+        return {sum, num};
+    }
+
+    double maximumAverageSubtree(TreeNode* root) {
+        int sum = 0, n = 0;
+        preorder(root);
+        return ans;
+    }
+};
