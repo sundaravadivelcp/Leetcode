ffc8d951edfb2aac0fcc639c0290c812360cb3d8
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2023-12-18 23:13:16 -0600

    Time: 46 ms (40%), Space: 22 MB (59.38%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.cpp
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
-        int rows = img.size();
-        int cols = img[0].size();
-        std::vector<std::vector<int>> result(rows, std::vector<int>(cols, 0));
+        int m = img.size(), n = img[0].size();
 
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                int total_sum = 0;
-                int count = 0;
-
-                for (int x = std::max(0, i-1); x < std::min(rows, i+2); ++x) {
-                    for (int y = std::max(0, j-1); y < std::min(cols, j+2); ++y) {
-                        total_sum += img[x][y];
-                        count += 1;
+        for(int i =0; i< m; i++){
+            for(int j = 0; j< n; j++){
+                int count = 0, sum = 0;
+                for(int x = max(0, i-1); x < min(m, i+2); x++){
+                    for(int y = max(0, j-1); y <min(n, j+2); y++){
+                        sum += img[x][y] & 255;
+                        count++;
                     }
+                    
                 }
-
-                result[i][j] = total_sum / count;
+                img[i][j] |= ((sum/count)<<8);
+            }
+        }
+        for(int i =0; i< m; i++){
+            for(int j = 0; j< n; j++){
+                img[i][j] >>= 8;
             }
         }
 
-        return result;
+        return img;
     }
 };
