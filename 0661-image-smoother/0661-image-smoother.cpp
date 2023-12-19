8ad9be574592fb0a3d4cd3f28af377f5f7269c68
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2023-12-18 23:07:29 -0600

    Time: 301 ms (5.23%), Space: 90.8 MB (5.54%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.cpp
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
         int m = img.size(), n = img[0].size();
-        vector<vector<int>> dir = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+        vector<vector<int>> dir = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,0},{0,1},{1,-1},{1,0},{1,1}};
         vector<vector<int>> ans(m,vector<int>(n));
 
         for(int i =0; i< m; i++){
             for(int j = 0; j< n; j++){
-                int count = 1, sum = img[i][j];
+                int count = 0, sum = 0;
                 for(auto v:dir){
                     int x = i + v[0], y = j + v[1];
-                    if(x>=0 && y>=0 && x<m && y<n){
-                        sum += img[x][y];
+                    if(x >= 0 && y >= 0 && x < m && y < n){
+                        sum += img[x][y] & 255;
                         count++;
                     }
                 }
-                ans[i][j] = sum/count;
+                img[i][j] |= ((sum/count)<<8);
             }
         }
-        return ans;
+        for(int i =0; i< m; i++){
+            for(int j = 0; j< n; j++){
+                img[i][j] >>= 8;
+            }
+        }
+
+        return img;
     }
 };
