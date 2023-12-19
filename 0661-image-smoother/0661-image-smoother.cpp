e2212710459d2261e80754ea1962fc08319cd907
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2023-12-18 23:09:28 -0600

    Time: 41 ms (69.85%), Space: 22.2 MB (33.54%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.cpp
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
         int m = img.size(), n = img[0].size();
-        vector<vector<int>> dir = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,0},{0,1},{1,-1},{1,0},{1,1}};
 
         for(int i =0; i< m; i++){
             for(int j = 0; j< n; j++){
