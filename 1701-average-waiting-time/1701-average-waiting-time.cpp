9eefae9a9272e0912236d353243302d9aeee5a13
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2023-12-21 00:49:38 -0600

    Time: 163 ms (87.44%), Space: 73.1 MB (79.82%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         double sum = 0;
-        int t = customers[0][0];
+        int t = 0;
 
         for(int  i = 0 ; i< customers.size(); i++){
             t = max(t, customers[i][0]) + customers[i][1];
