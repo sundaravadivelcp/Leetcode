35f6951d4c4d1f22d2c85250f2592aba63197d05
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2023-12-21 00:50:22 -0600

    Time: 207 ms (8.07%), Space: 83.2 MB (12.11%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -4,9 +4,9 @@ public:
         double sum = 0;
         int t = 0;
 
-        for(int  i = 0 ; i< customers.size(); i++){
-            t = max(t, customers[i][0]) + customers[i][1];
-            sum += t - customers[i][0];
+        for(auto customer:customers){
+            t = max(t, customer[0]) + customer[1];
+            sum += t - customer[0];
         }
         return sum/customers.size();
     }
