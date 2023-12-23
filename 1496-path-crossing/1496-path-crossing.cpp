719f20ac939f26491e4108699ed9fd3da1ebd7a0
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2023-12-22 19:38:23 -0600

    Time: 0 ms (100%), Space: 7.1 MB (64.64%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isPathCrossing(string path) {
-        unordered_map<char, pair<int,int>> m = {{'N', {0,-1}},{'E', {1,0}},{'S',{0,1}},{'W',{-1,0}}};
+        unordered_map<char, pair<int,int>> moves = {{'N', {0,-1}},{'E', {1,0}},{'S',{0,1}},{'W',{-1,0}}};
 
-        set<pair<int, int>> s;
+        set<pair<int, int>> visited;
         int x = 0, y = 0;
-        s.insert({x,y});
+        visited.insert({x,y});
 
         for(char c:path){
-            x += m[c].first;
-            y += m[c].second;
-            if(s.find({x,y}) != s.end()){
+            x += moves[c].first;
+            y += moves[c].second;
+            if(visited.find({x,y}) != visited.end()){
                 return true;
             }
-            s.insert({x,y});
+            visited.insert({x,y});
         }
         return false;
     }
