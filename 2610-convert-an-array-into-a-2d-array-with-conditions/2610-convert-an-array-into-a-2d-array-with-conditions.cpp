fdbc5814370bf3c4950d6fc6a9e772b54d21ce6f
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-01-01 20:39:22 -0600

    Time: 7 ms (90.89%), Space: 29.9 MB (83.6%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -3,7 +3,6 @@ public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> freq(nums.size() + 1);
-        sort(nums.begin(), nums.end());
         for(auto num:nums){
             if(freq[num] >= ans.size()){
                 ans.push_back({});
