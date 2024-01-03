3d11b2f9d38e1331ecd7cc5f87acffb3a858927a
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-01-02 20:10:10 -0600

    Time: 93 ms (47.47%), Space: 23.8 MB (44.69%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
         vector<int> v;
-        int ans = 0, n = bank.size();
         for(auto& b:bank){
             int count = 0;
             for(char& c:b){
@@ -17,6 +16,7 @@ public:
             return 0;
         }
 
+        int ans = 0;
         for(int i = 0; i< v.size() - 1; i++){
             ans += v[i] * v[i+1];
         }
