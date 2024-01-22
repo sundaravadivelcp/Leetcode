2d9ad176157dd9d666e38aeafbf6c8d366903d43
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-01-21 23:41:52 -0600

    Time: 32 ms (47.64%), Space: 24.8 MB (34.62%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -3,14 +3,15 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size(), repeatingNumber, missingNumber;
         sort(nums.begin(), nums.end());
-        int eSum = n*(n+1)/2, aSum = nums[0];
+        int eSum = n*(n+1)/2;
+        eSum -= nums[0];
         for(int i =1;i<n; i++){
-            aSum += nums[i];
+            eSum -= nums[i];
             if(nums[i] == nums[i-1]){
                 repeatingNumber = nums[i];
             }
         }
-        missingNumber = eSum - aSum + repeatingNumber;
+        missingNumber = eSum  + repeatingNumber;
 
         return {repeatingNumber, missingNumber};
     }
