cb331d601bc6874c7efbf30b9d6e1271fea8cf40
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-01-22 19:01:06 -0600

    Time: 0 ms (100%), Space: 9.8 MB (80.35%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,23 +1,57 @@
 class Solution {
 public:
-    int maxLength(vector<string>& arr) {
-        // Use depth first search recursion through arr
-        // building from an initial empty string
-        return dfs(arr, 0, "");
-    }
-
-private:
-    int dfs(vector<string>& arr, int pos, string res) {    
-        // Use a set to check res for duplicate characters
-        unordered_set<char> resSet(res.begin(), res.end());
-        if (res.size() != resSet.size())
-            return 0;
+    int maxLength(vector<string>& arr) {        
+        // Pre-process arr with an optimizing helper
+        // which converts each word to its character bitset
+        // and then uses a set to prevent duplicate results
+        unordered_set<int> optSet;
+        for (auto& word : arr) 
+            wordToBitSet(optSet, word);
         
-        // Recurse through each possible next option
-        // and find the best answer
-        int best = res.size();
-        for (int i = pos; i < arr.size(); i++)
-            best = max(best, dfs(arr, i + 1, res + arr[i]));
+        // Convert the set back to an array for iteration
+        // then start up the recursive helper
+        vector<int> optArr(optSet.begin(), optSet.end());
+        return dfs(optArr, 0, 0);
+    }
+    
+private:    
+    void wordToBitSet(unordered_set<int>& optSet, string& word) {        
+        // Initialize an empty int to use as a character bitset
+        int charBitSet = 0;
+        for (char& c : word) {
+            // If the bitset contains a duplicate character
+            // then discard this word with an early return
+            // otherwise add the character to the bitset
+            int mask = 1 << c - 'a';
+            if (charBitSet & mask)
+                return;
+            charBitSet += mask;
+        }
+        
+        // Store the length of the word in the unused space
+        // then add the completed bitset to our optimized set
+        optSet.insert(charBitSet + (word.length() << 26));
+    }
+    
+    int dfs(vector<int>& optArr, int pos, int res) {
+        // Separate the parts of the bitset res
+        int oldChars = res & ((1 << 26) - 1);
+        int oldLen = res >> 26;
+        int best = oldLen;
+        
+        // Iterate through the remaining results
+        for (int i = pos; i < optArr.size(); i++) {
+            int newChars = optArr[i] & ((1 << 26) - 1);
+            int newLen = optArr[i] >> 26;
+            
+            // If the two bitsets overlap, skip to the next result
+            if (newChars & oldChars)
+                continue;
+            
+            // Combine the two results and trigger the next recursion
+            int newRes = newChars + oldChars + (newLen + oldLen << 26);
+            best = max(best, dfs(optArr, i + 1, newRes));
+        }
         return best;
     }
 };
