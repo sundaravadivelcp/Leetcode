48002a356931e292fa9736c66f3ad87b6cf14f16
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-01-22 18:09:22 -0600

    Time: 15 ms (97.38%), Space: 23.8 MB (38.12%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size(), repeatingNumber, missingNumber;
-        for(int i = 0;i<n; i++){
+        int repeatingNumber, missingNumber;
+        for(int i = 0;i<nums.size(); i++){
             if(nums[abs(nums[i]) - 1] < 0){
                 repeatingNumber = abs(nums[i]);
             } else {
                 nums[abs(nums[i]) - 1] *= -1;
             }
         }
-        for(int i = 0;i<n; i++){
+        for(int i = 0;i<nums.size(); i++){
             if(nums[i] > 0){
                 missingNumber = i + 1;
             }
