856c7e9caa6b9370a6400c5a9c43dfab98488fd9
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-01-22 18:08:47 -0600

    Time: 11 ms (99.67%), Space: 23.8 MB (36.68%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0;i<n; i++){
             if(nums[abs(nums[i]) - 1] < 0){
                 repeatingNumber = abs(nums[i]);
-            }else {
+            } else {
                 nums[abs(nums[i]) - 1] *= -1;
             }
         }
