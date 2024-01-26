13183bbb837ecdf67d919921fab19c98d574260d
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-01-25 18:18:36 -0600

    Time: 62 ms (78.19%), Space: 27.6 MB (26.2%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int connectSticks(vector<int>& sticks) {
-        int n = sticks.size();
-        if(n == 1){
-            return 0;
-        }
         priority_queue<int,vector<int>, greater<int>> pq;
-        int ans = 0;
+        int totalCost = 0;
         for(int& stick:sticks){
             pq.push(stick);
         }
@@ -15,9 +11,9 @@ public:
             int stick1 = pq.top();pq.pop();
             int stick2 = pq.top();pq.pop();
             pq.push(stick1 + stick2);
-            ans += stick1 + stick2;
+            totalCost += stick1 + stick2;
         }
 
-        return ans;
+        return totalCost;
     }
 };
