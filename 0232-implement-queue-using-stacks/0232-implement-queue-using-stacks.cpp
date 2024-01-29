a1ca9aa24a4570990631eeb0a1c96913bff10f3d
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-01-28 22:21:42 -0600

    Time: 0 ms (100%), Space: 8.3 MB (15.21%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,10 +1,14 @@
 class MyQueue {
     stack<int> s1,s2;
+    int front;
 public:
     MyQueue() {
     }
     
     void push(int x) {
+        if(s1.empty()){
+            front = x;
+        }
         s1.push(x);
     }
     
@@ -15,6 +19,11 @@ public:
         }
         int top = s2.top();
         s2.pop();
+        if(s2.empty()){
+            front = -1;
+        } else {
+            front = s2.top();
+        }
 
         while(!s2.empty()){
             s1.push(s2.top());
@@ -24,18 +33,7 @@ public:
     }
     
     int peek() {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
-        }
-        int top = s2.top();
-
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
-        return top;
-        
+        return front;
     }
     
     bool empty() {
