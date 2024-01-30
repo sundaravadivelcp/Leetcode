b666e1671c3b39ce5246ef764917777c801bf4ac
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-01-30 01:09:42 -0600

    Time: 7 ms (65.84%), Space: 12.5 MB (14.89%) - LeetHub

--- 0256-paint-house/0256-paint-house.cpp
+++ 0256-paint-house/0256-paint-house.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minCost(vector<vector<int>>& matrix) {
-        int n = 3, m = matrix.size();
-        vector<int> dp(n);
-        vector<int> row(n);
+        int m = matrix.size();
+        vector<int> dp(3);
+        vector<int> row(3);
 
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<3; i++){
             dp[i] = matrix[0][i];
         }
 
         for(int i = 1; i < m; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < 3; j++){
                 int prev = INT_MAX;
                 if(j==0){
                     row[j] = min(dp[j+1], dp[j+2]) + matrix[i][j];
-                } else if(j == n-1){
+                } else if(j == 2){
                     row[j] = min(dp[j-2], dp[j-1]) + matrix[i][j];
                 } else {
                     row[j] = min(dp[j-1], dp[j+1]) + matrix[i][j];
@@ -25,7 +25,7 @@ public:
 
         int ans = INT_MAX;
         
-        for(int i = 0; i<n; i++){       
+        for(int i = 0; i<3; i++){       
             ans = min(ans, dp[i]);
         }
 
