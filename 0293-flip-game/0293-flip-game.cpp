124e7034b2b5567fca9d59669bd018956d68e321
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-02-01 18:33:36 -0600

    Time: 5 ms (15.96%), Space: 8.5 MB (20.21%) - LeetHub

--- 0293-flip-game/0293-flip-game.cpp
+++ 0293-flip-game/0293-flip-game.cpp
@@ -4,11 +4,12 @@ public:
         int n = currentState.length();
         vector<string> answer;
         for(int i = 0; i<n-1; i++){
-            string temp = currentState;
             if(currentState[i] == '+' && currentState[i+1] == '+'){
-                temp[i] = '-';
-                temp[i+1] = '-';
-                answer.push_back(temp);
+                currentState[i] = '-';
+                currentState[i+1] = '-';
+                answer.push_back(currentState);
+                currentState[i] = '+';
+                currentState[i+1] = '+';
             }
         }
         return answer;
