503375d7972df777a8eabf9f23882b312c15c2aa
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-02-01 19:40:30 -0600

    Time: 3 ms (17.15%), Space: 7.3 MB (13.81%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
         vector<int> answer;
+        string seq = "123456789";
         int minLen = to_string(low).length(), maxLen = to_string(high).length();
-        int num = 0;
-        for(int i = 1; i<10;i++){
-            num = i;
-            for(int j = minLen; j <= maxLen; j++){
-                for(int k = num%10 + 1; k < 10; k++){
-                    num *= 10;
-                    num += k;
-                    if(num >= low && num <= high){
-                        answer.push_back(num);
-                    }
+
+        for(int i = minLen; i <= maxLen; i++){
+            for(int j = 0; j < seq.size() - i + 1; j++){
+                int num = stoi(seq.substr(j,i));
+                if(num >= low && num <= high){
+                    answer.push_back(num);
                 }
             }
-            
         }
-        sort(answer.begin(), answer.end());
 
         return answer;
     }
