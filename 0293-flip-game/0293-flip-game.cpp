bf3dec2ebbdd7cb1368682392146f5a998f46f84
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-02-01 18:34:12 -0600

    Time: 0 ms (100%), Space: 8.4 MB (23.4%) - LeetHub

--- 0293-flip-game/0293-flip-game.cpp
+++ 0293-flip-game/0293-flip-game.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<string> generatePossibleNextMoves(string currentState) {
-        int n = currentState.length();
         vector<string> answer;
-        for(int i = 0; i<n-1; i++){
+        for(int i = 0; i < currentState.length()-1; i++){
             if(currentState[i] == '+' && currentState[i+1] == '+'){
                 currentState[i] = '-';
                 currentState[i+1] = '-';
