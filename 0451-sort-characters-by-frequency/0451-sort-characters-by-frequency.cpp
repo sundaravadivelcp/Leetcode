86ea878cad048f2dcb008ea3b5bb8b7eb167e211
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-02-06 18:18:24 -0600

    Time: 16 ms (19.18%), Space: 10.8 MB (21.89%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        string ans;
+        map<char,int> mp;
+        for(int i =0; i<s.size(); i++){
+            mp[s[i]]++;
+        }
+        multimap<int, char> mm;
+ 
+        // Insert every (key-value) pairs from
+        // map M to multimap MM as (value-key)
+        // pairs
+        for (auto& it : mp) {
+            mm.insert({ it.second, it.first });
+        }
+
+        for (auto it = mm.rbegin(); it != mm.rend(); ++it) {
+            int count = it->first;
+            while(count--)
+                ans += it->second;
+        }
+
+        return ans;
+    }
+};
