a85182c26003e8a62e66baad2730ab90a63d8120
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-02-09 15:52:44 -0600

    Time: 7 ms (41.59%), Space: 16.3 MB (5.18%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,11 +5,12 @@ public:
         if(n == 1){
             return nums[0];
         }
-        int min_so_far = nums[0], max_so_far = nums[0], result = nums[0];
+        int min_so_far = nums[0], max_so_far = min_so_far, result = min_so_far;
 
         for(int i = 1; i<nums.size(); i++){
-            int tempMax = max(nums[i], max(nums[i]* max_so_far, nums[i]* min_so_far));
-            min_so_far = min(nums[i], min(nums[i]* max_so_far, nums[i]* min_so_far));
+            int cur = nums[i];
+            int tempMax = max(cur, max(cur* max_so_far, cur* min_so_far));
+            min_so_far = min(cur, min(cur* max_so_far, cur* min_so_far));
 
             max_so_far = tempMax;
             result = max(result, max_so_far);
