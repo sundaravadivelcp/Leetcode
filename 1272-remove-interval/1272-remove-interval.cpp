cd8de4a5213c3ea36335e085d6e75b6d67a25d04
Sundaravadivel CP <sundaravadivelcp@gmail.com>
2024-02-16 13:45:01 -0600

    Time: 68 ms (69.87%), Space: 41.8 MB (51.99%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.cpp
+++ 1272-remove-interval/1272-remove-interval.cpp
@@ -5,16 +5,12 @@ public:
 
         for(auto& interval:intervals){
             int s = interval[0], e = interval[1];
-            if(s < toBeRemoved[0] && e <= toBeRemoved[0] || s >= toBeRemoved[1] && e >= toBeRemoved[1]){
+            if(e <= toBeRemoved[0] || s >= toBeRemoved[1]){
                 ans.push_back({s,e});
-            } else if(s < toBeRemoved[0]){
-                if(e <= toBeRemoved[1]){
+            } else {
+                if(s < toBeRemoved[0]){
                     ans.push_back({s, toBeRemoved[0]});
-                } else {
-                    ans.push_back({s, toBeRemoved[0]});
-                    ans.push_back({toBeRemoved[1], e});
                 }
-            } else if(s >= toBeRemoved[0] && s < toBeRemoved[1] ){
                 if(e > toBeRemoved[1]){
                     ans.push_back({toBeRemoved[1], e});
                 }
